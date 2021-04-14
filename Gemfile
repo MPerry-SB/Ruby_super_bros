684d7725dc22ba19afbf25ad2695824484126b7d
John Gallagher <john.gallagher@simplybusiness.co.uk>
2021-04-14 20:15:05 +0100

    test: decouple from Window

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'ruby2d'
 gem "minitest", "~> 5.14"
+
+gem "rspec-mocks", "~> 3.10"
--- test/super_ruby_bros_test.rb
+++ test/super_ruby_bros_test.rb
@@ -1,21 +1,35 @@
 require 'minitest/autorun'
+require 'rspec/mocks/minitest_integration'
 require_relative '../lib/super_ruby_bros'
 
-class Window
-  class << self
-    attr_reader :objects
+class FakeWindow
+  def set args
   end
 
-  def self.add(args)
-    @objects ||= []
-    @objects << args
+  def show
+  end
+
+  def height
+    200
+  end
+
+  def width
+    300
+  end
+
+  def on arg
+  end
+
+  def update
   end
 end
 
 class SuperRubyBrosTest < Minitest::Test
   def test_game_boots
+    fake_window = FakeWindow.new
+    stub_const("Window", fake_window)
     game = SuperRubyBros.new
     game.run
-    assert_equal [], Window.objects
+    assert true
   end
 end

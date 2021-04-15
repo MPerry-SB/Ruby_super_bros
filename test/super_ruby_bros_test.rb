6af29a269cf5ca2b749f50aa8305fb91e2d97398
John Gallagher <john.gallagher@simplybusiness.co.uk>
2021-04-15 12:57:13 +0100

    test: test that the game starts with entities

--- test/super_ruby_bros_test.rb
+++ test/super_ruby_bros_test.rb
@@ -66,11 +66,44 @@ class SuperRubyBrosTest < Minitest::Test
     assert fake_window.objects.one?(&hero?)
   end
 
+  def test_game_shows_all_assets_on_launch
+    fake_window = FakeWindow.new
+
+    stub_const("Window", fake_window)
+    stub_const("Ruby2D::Window", fake_window)
+
+    game = SuperRubyBros.new
+    game.run
+
+    assert_equal 20, fake_window.objects.count
+    assert_equal 8, fake_window.objects.count(&wood?)
+    assert_equal 1, fake_window.objects.count(&enemy?)
+    assert_equal 1, fake_window.objects.count(&enemy_collision_detection_square?)
+  end
+
   private
 
   def hero?
     lambda do |object|
       object.is_a?(Ruby2D::Sprite) && object.path == './assets/hero.png'
+    end
+  end
+
+  def wood?
+    lambda do |object|
+      object.is_a?(Ruby2D::Image) && object.path == './assets/wood.png'
+    end
+  end
+
+  def enemy?
+    lambda do |object|
+      object.is_a?(Ruby2D::Image) && object.path == './assets/crab.png'
+    end
+  end
+
+  def enemy_collision_detection_square?
+    lambda do |object|
+      object.is_a?(Ruby2D::Square)
     end
   end
 end

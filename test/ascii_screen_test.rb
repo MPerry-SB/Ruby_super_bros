28eebd8dca1e87e2e9218575b69ca3c5034018df
John Gallagher <john.gallagher@simplybusiness.co.uk>
2021-04-20 19:01:07 +0100

    test: allow scaling pixel heights

--- test/ascii_screen_test.rb
+++ test/ascii_screen_test.rb
@@ -4,6 +4,13 @@ require 'ostruct'
 class AsciiScreen
   def initialize ascii_art, width:, height:
     @ascii_art = ascii_art
+    @width = width
+    @height = height
+    @number_of_rows = ascii_art.split("\n").count
+    @number_of_columns = ascii_art.split("\n").first.chars.count
+
+    @pixel_width = @height / @number_of_rows
+    @pixel_height = @width / @number_of_columns
   end
 
   def to_a
@@ -14,7 +21,7 @@ class AsciiScreen
         row
           .chars
           .each_with_index.map do |_, column_index|
-            OpenStruct.new(path: './assets/wood.png', x: column_index, y: row_index)
+            OpenStruct.new(path: './assets/wood.png', x: column_index * @pixel_width, y: row_index * @pixel_height)
           end
       end
   end
@@ -43,6 +50,15 @@ class AsciiScreenTest < Minitest::Test
     expected_objects = [
       OpenStruct.new(path: './assets/wood.png', x: 0, y: 0),
       OpenStruct.new(path: './assets/wood.png', x: 0, y: 1)
+    ]
+    assert_equal expected_objects, objects
+  end
+
+  def test_two_sprites_return_different_x_and_ys_with_double_height
+    objects = AsciiScreen.new("-\n-", width: 2, height: 4).to_a
+    expected_objects = [
+      OpenStruct.new(path: './assets/wood.png', x: 0, y: 0),
+      OpenStruct.new(path: './assets/wood.png', x: 0, y: 2)
     ]
     assert_equal expected_objects, objects
   end
